9222b3dad016c443ca7d4645ca0fa8a2bd3a2305
TachoBlade <47395463+AdityaPrasad275@users.noreply.github.com>
2022-07-28 17:46:30 +0530

    Create Wierd_Algorithm.cpp

---
+++ CSES Problem Set/Introductory Problems/Wierd_Algorithm.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<set>
+ 
+int main() {
+	long long int n;
+	std::cin >> n;
+	while (n != 1) {
+		std::cout << n << " ";
+		if (n % 2 == 0) n = n / 2;
+		else n = 3 * n + 1;
+	}
+	std::cout << 1;
+}
